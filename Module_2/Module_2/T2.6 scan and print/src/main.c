2c23e5b31f7ba669e0e487c93104dbdb87e6e026
unknown <aapo_koskinen@hotmail.com>
2023-06-09 01:50:53 +0300

    asd

--- Module_2/Module_2/T2.6 scan and print/src/main.c
+++ Module_2/Module_2/T2.6 scan and print/src/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main(void)
-{
+{   
+
     int a;
-    int f;
-    int ret_a = scanf("");
-    printf("Integer is %d\nFloating-point number: %f\n", a, f);
+    float f;
+    int ret = scanf("%d %f", &a, &f);
+    printf("Captured %d\n", ret);
+    printf("Integer is 0x%08X\n",a);
+    printf("Floating-point number is %8.3f\n",f);
     
     return 0;
 }

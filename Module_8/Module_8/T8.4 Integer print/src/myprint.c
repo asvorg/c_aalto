2d3c207679883b92ce4a73be0d9311c6624fdbea
unknown <aapo_koskinen@hotmail.com>
2023-07-18 21:56:53 +0300

    exercise 8.4

--- Module_8/Module_8/T8.4 Integer print/src/myprint.c
+++ Module_8/Module_8/T8.4 Integer print/src/myprint.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdarg.h>
 
+int myprint(const char *str, ...){
+    va_list args;
+    va_start(args, str);
+    int i = 0;
+    int res = 0;
+    while(str[i] != '\0'){
+        int num;
+        if(str[i] == '&'){
+            num = va_arg(args, int);
+            printf("%d", num);
+            res++;
+        }else{
+            printf("%c", str[i]);
+        }
+        i++;
+    }
+    va_end(args);
+    return res;
+}
